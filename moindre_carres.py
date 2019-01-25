b8ec371722b6b984c9286f5aeccc86e8ae37fa0b
Cockong <45364934+Cockong@users.noreply.github.com>
2019-01-25 10:30:56 +0100

    Add files via upload

--- moindre_carres.py
+++ moindre_carres.py
@@ -9,6 +9,7 @@ import csv
 import numpy as np
 import matplotlib as plt
 import pylab as pl
+import random
 
 def open_numeric(baseFileName, fieldnames=['user','movie','rating','datestamp'], delimiter='\t'):
     """
@@ -24,7 +25,7 @@ def open_file(baseFileName, fieldnames=None , delimiter='|'):
     """
     fonction généralisée pour lire les fichiers non numériques
     """   
-    with open(baseFileName, 'r') as f:
+    with open(baseFileName, 'r',encoding = "ISO-8859-1") as f:
          reader = csv.DictReader(f, delimiter = delimiter,fieldnames=fieldnames)
          return list(reader)
 
@@ -42,6 +43,14 @@ def calcul_MAE(Rtest, matrice_prediction):
                 errorRating.append(matrice_prediction[i][j] - Rtest[i][j])
     return(np.mean(np.abs(errorRating)))
 
+def resolution_equa_normale(A,b):
+    #methode pour inverser dapres le cours
+
+
+    AT = A.T
+    z = A.T.dot(b)
+    Inv = np.linalg.inv(AT.dot(A))
+    return Inv.dot(z) # ne fonctionne que si A inversible cequi nest pas notre cas
 
 
 baseUserItem=open_numeric("u1.base")
@@ -61,30 +70,74 @@ NbUsers = len(User)
 NbItems= len(Item)
 m=NbUsers # PAS SUR A 100% DE NE PAS AVOIR INVERSER
 n=NbItems
+Rtest = np.zeros((NbUsers, NbItems))
 R = np.zeros((NbUsers, NbItems))
 W = np.zeros((NbUsers, NbItems))
 for row in baseUserItem:
     R[row['user']-1,row['movie']-1] = row['rating']
     W[row['user']-1,row['movie']-1] = 1
+for row in testUserItem:
+    Rtest[row['user']-1,row['movie']-1] = row['rating']
 #pl.imshow(R,interpolation='none')
 
+def prediction_moindrecarre(R, W, Rtest, nbrIter = 15 ,nbrFeatures = 1 ,lmbd = 0.1, ):
+    """    
+    nbrIter =  nombre d'iterations
+    nbrFeatures = k
+    lmbd  = parametre lambda
+    """
+    score=[]
+    m=R.shape[0]
+    n=R.shape[1]
+    X = np.ones( (m, nbrFeatures) ) # matrice initiale X
+    Y = np.ones( (nbrFeatures, n ) ) # matrice initiale Y
+    #X = np.random.rand( m, nbrFeatures ) # matrice initiale X
+    #Y = np.random.rand( nbrFeatures, n ) # matrice initiale Y
+    #X2 = np.ones( (m, nbrFeatures) )
+    
+    Ik = np.eye( nbrFeatures )
+    alpha= lmbd * Ik # éviter de recalculer
+    mean_items=[0 for row in range(NbItems)] #liste des moyenne par colonne(film)
+    for col in range(NbItems):
+       rating_number=0 #savoir le nombre d'éléments quon additionne pour diviser la moyenne 
+       for user_rating in R[:,col]:
+           if user_rating!=0: # on ne prend que les valeurs non nul
+               mean_items[col]+=user_rating
+               rating_number+=1
+       if rating_number!=0: #il doit y avoir au moins une note par film
+           mean_items[col]=mean_items[col]/rating_number 
+    
+    Y=np.array([mean_items for k in range(nbrFeatures)]) #approximer par la moyenne
 
+          
 
-
+    for j in range(nbrIter):
+        
+        for u in range(m):
+            Wu = np.diag(W[u])
+            A = Y.dot(Wu).dot(Y.T) + alpha
+            b = Y.dot(Wu).dot(R[u].T)
+            #attention cest X[u].T quon a calculé je ne sais i ca pose probleme
+            #X[u]=np.linalg.solve(A,b) # ne fonctionne que si A inversible cequi nest pas notre cas
+            X[u]=resolution_equa_normale(A,b) # ne fonctionne que si A inversible cequi nest pas notre cas
 
     
-nbrIter = 10 # nombre d'iterations
-nbrFeatures = 1 #== k / ne marche que pour k=1
-lmbd = 0.02 # parametre lambda
-#X = np.ones( (m, nbrFeatures) ) # matrice initiale X
-#Y = np.ones( (nbrFeatures, n) ) # matrice initiale Y
-X2 = np.ones( (m, nbrFeatures) )
+        for i in range(n):
+            Wi = np.diag(W[:,i])
+            A = X.T.dot(Wi).dot(X) + alpha 
+            b = X.T.dot(Wi).dot(R[:,i])
+            #Y[:,i]=np.linalg.solve(A,b) # ne fonctionne que si A inversible cequi nest pas notre cas
+            Y[:,i]=resolution_equa_normale(A,b) # ne fonctionne que si A inversible cequi nest pas notre cas
 
-Ik = np.ones( (nbrFeatures, nbrFeatures) )
-alpha= lmbd * Ik # éviter de recalculer
-
-#approximation de X et Y par la moyenne 
-
+        
+        P=X.dot(Y) #     Calculer la prediction pour chaque rating du jeu de données test
+        score.append(calcul_MAE(Rtest, P))
+        
+    return score
+        
+score=prediction_moindrecarre(R,W,Rtest)
+"""
+#approximation de X et Y par la moyenne
 #fonctionne moins bien meilleur départ mais cnvergence moins bonne HYPOTHESE (nameliore rien)=> apparemment il ya des 0 dans Y : des films nont pas été notés !!! je vais essayer de remplacer ces zeros par des 1
 
 mean_users=[0 for row in range(NbUsers)] #liste des moyenne par lignes(users)
@@ -97,6 +150,10 @@ for row in range(NbUsers):
    if rating_number!=0:    #il doit y avoir au moins une note par film   
        mean_users[row]=mean_users[row]/rating_number      
 
+X=np.array([mean_users for k in range(nbrFeatures)]) #approximer par la moyenne
+
+X=X.T
+ 
 mean_items=[0 for row in range(NbItems)] #liste des moyenne par colonne(film)
 for col in range(NbItems):
    rating_number=0 #savoir le nombre d'éléments quon additionne pour diviser la moyenne 
@@ -107,12 +164,18 @@ for col in range(NbItems):
    if rating_number!=0: #il doit y avoir au moins une note par film
        mean_items[col]=mean_items[col]/rating_number 
 
-X=np.array([mean_users for k in range(nbrFeatures)]) #approximer par la moyenne
 Y=np.array([mean_items for k in range(nbrFeatures)]) #approximer par la moyenne
-X=X.T
-            
+
+          
 
 for j in range(nbrIter):
+    
+    for u in range(m):
+        Wu = np.diag(W[u])
+        A = Y.dot(Wu).dot(Y.T) + alpha
+        b = Y.dot(Wu).dot(R[u].T)
+        #attention cest X[u].T quon a calculé je ne sais i ca pose probleme
+        X[u]=np.linalg.solve(A,b) # ne fonctionne que si A inversible cequi nest pas notre cas
 
     for i in range(n):
         Wi = np.diag(W[:,i])
@@ -120,32 +183,13 @@ for j in range(nbrIter):
         b = X.T.dot(Wi).dot(R[:,i])
         Y[:,i]=np.linalg.solve(A,b) # ne fonctionne que si A inversible cequi nest pas notre cas
         
-    for u in range(m):
-        Wu = np.diag(W[u])
-        A = Y.dot(Wu).dot(Y.T) + alpha
-        b = Y.dot(Wu).dot(R[u].T)
-        #attention cest X[u].T quon a calculé je ne sais i ca pose probleme
-        X[u]=np.linalg.solve(A,b) # ne fonctionne que si A inversible cequi nest pas notre cas
+
     
     P=X.dot(Y) #     Calculer la prediction pour chaque rating du jeu de données test
-    score=calcul_MAE(R, P)
+    score=calcul_MAE(Rtest, P)
     print(score)
     
 
-"""
-
-    
-#methode pour inverser dapres le cours
-for u in range(m):
-    print(u)
-    Wu = np.diag(W[u])
-    A = Y.dot(Wu).dot(Y.T) + alpha
-    b = Y.dot(Wu).dot(R[u].T)
-    #attention cest X[u].T quon a calculé je ne sais i ca pose probleme
-    AT = A.T
-    z = A.T.dot(b)
-    Inv = np.linalg.inv(AT.dot(A))
-    X2[u] = Inv.dot(z) # ne fonctionne que si A inversible cequi nest pas notre cas
     
 #test comparaison des deux méthodes    
     
